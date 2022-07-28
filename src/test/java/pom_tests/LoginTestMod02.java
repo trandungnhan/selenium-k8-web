d4080d49e3aa02631490851f956bfb131ccf7a38
Tran Dung Nhan <trandungnhan89@gmail.com>
2022-07-28 22:45:55 +0700

    Lesson 19

---
+++ src/test/java/pom_tests/LoginTestMod02.java
@@ -0,0 +1,31 @@
+package pom_tests;
+
+import driver.DriverFactory;
+import models.pages.LoginPageMod02;
+import org.openqa.selenium.WebDriver;
+
+public class LoginTestMod02 {
+
+    public static void main(String[] args) {
+
+        // Get a chrome session
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try {
+            // Navigate to the target page
+            driver.get("https://the-internet.herokuapp.com/login");
+
+            LoginPageMod02 loginPage = new LoginPageMod02(driver);
+            loginPage.inputUsername("teo@sth.com");
+            loginPage.inputPassword("12345678");
+            loginPage.clickOLoginBtn();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // Quit the browser session
+        driver.quit();
+
+    }
+}
