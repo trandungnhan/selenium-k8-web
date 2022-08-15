a6ec8d5f0d56400281b9d595588f4f94c5d54ad8
Tran Dung Nhan <trandungnhan89@gmail.com>
2022-08-15 17:28:39 +0700

    lesson 24

---
+++ src/test/java/test_flows/computer/OrderComputerFlow.java
@@ -0,0 +1,34 @@
+package test_flows.computer;
+
+import models.components.order.ComputerEssentialComponent;
+import models.pages.ComputerItemDetailsPage;
+import org.openqa.selenium.WebDriver;
+
+public class OrderComputerFlow<T extends ComputerEssentialComponent> {
+
+    private final WebDriver driver;
+    private final Class<T> computerEssentialComponent;
+
+    public OrderComputerFlow(WebDriver driver, Class<T> computerEssentialComponent) {
+        this.driver = driver;
+        this.computerEssentialComponent = computerEssentialComponent;
+    }
+
+    public void buildCompSpecAndAddToCart(){
+        ComputerItemDetailsPage computerItemDetailsPage = new ComputerItemDetailsPage(driver);
+        T computerEssentialComp = computerItemDetailsPage.computerComp(computerEssentialComponent);
+        computerEssentialComp.selectProcessorType("2.2GHz");
+        computerEssentialComp.selectRAMType("8GB");
+//        computerEssentialComp.selectProcessorType("Fast");
+//        computerEssentialComp.selectRAMType("4 GB");
+
+        try{
+            Thread.sleep(5000);
+        } catch (Exception ignored){
+
+        }
+
+    }
+
+
+}
